0d2888d2de7cc4294211c51195c9bfe08ec63a39
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2023-06-15 13:32:11 +0700

    done

--- src/routers/iuran_bulanan_router.js
+++ src/routers/iuran_bulanan_router.js
@@ -2,10 +2,10 @@ const iuran_bulanan_controller = require("../controllers/iuran_bulanan_controlle
 const { jwtMiddleware } = require("../middlewares/jwt_middleware");
 const router = require("express")();
 
+router.get("/total-perbulan", iuran_bulanan_controller.totalPerBulan);
 router.use(jwtMiddleware);
 router.post("/bulk", iuran_bulanan_controller.bulk);
 router.get("/", iuran_bulanan_controller.list);
-router.get("/total-perbulan", iuran_bulanan_controller.totalPerBulan);
 router.get("/:id", iuran_bulanan_controller.detail);
 router.put("/edit/:id", iuran_bulanan_controller.edit);
 router.delete("/delete/:id", iuran_bulanan_controller.delete);
